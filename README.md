c317e5d0c1c2cf756f72ee15fc2deead7e8cb01f
david.schmidt@imagepixel.de <post@imagepixel.de>
2016-07-27 14:39:13 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# WebDev
+<a target="_blank" href="http://www.imagepixel.de"><img src="http://www.imagepixel.de/img/logo.png" alt="www.magepixel.de"></a>
+
+
+Build your own Bootstrap-sass framework.<br>
+Take the parts you really need and shrink your dist. css file.<br><br>
+
+it´s a small example for building your own Bootstrap framework with nodeJS (gulp, autoprefixer ...).<br>
+Bootstrap v3.3.6 <br>
+
+1. install nodeJS <br>
+2. change in this directory (in nodeJS command prompt) <br>
+3. type: `npm install` <br>
+4. gulp the gulpfile.js (type `gulp` or 'gulp watch' to do it automaticly)<br>
+5. edit src/sass/style.scss <br>
+6. edit src/js/files or add a few of them
+7. add a few images in src/img/
+8. edit src/index.php or add a few new files .html or .php
+9. find the new files in build/ folder
+10. enjoy ;)
+
+
+
+
---
+++ gulpfile.js
@@ -0,0 +1,85 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var imagemin = require('gulp-imagemin');
+var rename = require('gulp-rename');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var gulpif = require('gulp-if');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var htmlmin = require('gulp-htmlmin');
+
+
+var MODE = "production"; // oder 'development'
+
+//Bilder verkleinern
+gulp.task('images', function() {
+  return gulp.src('src/img/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('build/assets/images'));
+});
+
+//CSS verkleinern und autoprefixen
+gulp.task('css', function() {
+		// je nach Modus wird dementsprechend die CSS Datei erstellt.
+			if (MODE ==='production') {
+			var cssmode = 'compressed';
+			}else{cssmode='expanded';}
+
+	return gulp.src('src/sass/**/*.scss')
+		// on error wird benötigt wegen Fehlerausgabe
+		.pipe(sass({outputStyle: cssmode}).on('error', sass.logError))
+		.pipe(gulpif(MODE === 'production',
+			postcss([
+				autoprefixer({
+					browsers: ['last 2 version', 'ie 9', 'ie 10']
+				})
+			
+		])))
+		.pipe(rename('main.css'))
+		.pipe(gulp.dest('build/assets/css'));
+});
+
+//js verkleinern
+
+gulp.task('js', function() {
+	if (MODE ==='production') {
+		//production
+		 return gulp.src('src/js/**/*.js')
+		.pipe(concat('all.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('build/assets/js/'))
+			}else{
+				//development
+				return gulp.src('src/js/**/*.js')
+				.pipe(concat('all.min.js'))
+				.pipe(gulp.dest('build/assets/js/'))
+			}
+		
+});
+
+//html verkleinern
+gulp.task('minhtml', function() {
+  	if (MODE ==="production"){
+  		return gulp.src('src/*.{html,php}')
+    	.pipe(htmlmin({collapseWhitespace: true, useShortDoctype: true, html5: true}))
+    	.pipe(gulp.dest('build'));
+  	}else{
+  		return gulp.src('src/*.{html,php}')
+    	.pipe(htmlmin({collapseWhitespace: false, useShortDoctype: false, html5: false}))
+    	.pipe(gulp.dest('build'));
+  	}
+
+  	
+});
+
+
+//Watch Tasks erstellen. $ gulp watch aufrufen
+gulp.task('watch', function() {
+	gulp.watch('src/img/*.{png,jpg,gif}', ['images']);
+	gulp.watch('src/sass/**/*.scss', ['css']);
+	gulp.watch('src/js/**/*.js', ['js']);
+
+});
+
+gulp.task('default', ['images', 'css', 'js', 'minhtml']);
